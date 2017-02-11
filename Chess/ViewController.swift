6da92a8d3967618842ff96be04193f242a88b871
Jeremie Benhamron <jeremiebenhamron@gmail.com>
2017-02-10 18:13:24 -0800

    Initial Commit

---
+++ Chess/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chess
+//
+//  Created by Jeremie Benhamron on 2017-02-10.
+//  Copyright © 2017 beenie.inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
