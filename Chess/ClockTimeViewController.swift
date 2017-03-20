adaf2fd33c7c3431bc03cb90cc3470f6081599ea
Jeremie Benhamron <jeremiebenhamron@gmail.com>
2017-03-20 12:49:15 -0700

    Changed time control to clock time view controller

--- Chess/ClockTimeViewController.swift
+++ Chess/ClockTimeViewController.swift
@@ -1,27 +1,27 @@
 //
-//  TimeControlViewController.swift
+//  ClockTimeViewController.swift
 //  Chess
 //
-//  Created by Jeremie Benhamron on 2017-03-16.
+//  Created by Jeremie Benhamron on 2017-03-20.
 //  Copyright © 2017 beenie.inc. All rights reserved.
 //
 
 import UIKit
 
-class TimeControlViewController: IntegerSliderViewController {
+class ClockTimeViewController: IntegerSliderViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     override var integerDataDisplayer:(Int)->String{
         get{
             return {data in return self.display(minutesData: data)}
@@ -35,16 +35,7 @@ class TimeControlViewController: IntegerSliderViewController {
         return timeFormatter.hoursMinutesString
     }
     
-    private var interpretedData:ClockTime{
-        var timeFormatter = TimeFormatter()
-        timeFormatter.totalSeconds = integerData * 60
-         let (h,m,_) = timeFormatter.hoursMinutesAndSeconds
-        return ClockTime(hours:h,minutes:m)
+    private var interpretedData:TimeInterval{
+        return TimeInterval(integerData * 60)
     }
-    
-    struct ClockTime{
-        let hours:Int
-        let minutes:Int
-    }
-
 }
