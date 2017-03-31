610d05a477245af0c9be9523565bff9d54c06ae0
Jeremie Benhamron <jeremiebenhamron@gmail.com>
2017-03-30 17:28:10 -0700

    Started Creating SavedGames Scene

---
+++ Chess/SavedGameCell.swift
@@ -0,0 +1,31 @@
+//
+//  SavedGameCell.swift
+//  Chess
+//
+//  Created by Jeremie Benhamron on 2017-03-30.
+//  Copyright © 2017 beenie.inc. All rights reserved.
+//
+
+import UIKit
+
+class SavedGameCell: UITableViewCell {
+    //MARK: - Outlets
+    @IBOutlet weak var gameNumberLabel: UILabel!
+    @IBOutlet weak var playersLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    
+
+}
---
+++ Chess/SavedGamesTableViewController.swift
@@ -0,0 +1,98 @@
+//
+//  SavedGamesTableViewController.swift
+//  Chess
+//
+//  Created by Jeremie Benhamron on 2017-03-30.
+//  Copyright © 2017 beenie.inc. All rights reserved.
+//
+
+import UIKit
+
+class SavedGamesTableViewController: UITableViewController {
+
+    struct StoryBoard{
+        static let cellID = "savedGame"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.setNavigationBarHidden(false, animated: true)
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: StoryBoard.cellID, for: indexPath)
+        if let savedGameCell = cell as? SavedGameCell{
+            savedGameCell.gameNumberLabel.text = "Game11"
+            savedGameCell.playersLabel.text = "Beenie vs. Maxime"
+            savedGameCell.dateLabel.text = "2017-03-15"
+            savedGameCell.timeLabel.text = "9:45 AM"
+        }
+        
+
+        return cell
+    }
+
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
