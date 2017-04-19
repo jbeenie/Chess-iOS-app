53045763c6cfb45a6ca6bea4d0466c62a295f23c
Jeremie Benhamron <jeremiebenhamron@gmail.com>
2017-04-18 17:17:42 -0700

    implemented and tested deleting of games

--- Chess/SavedGamesTableViewController.swift
+++ Chess/SavedGamesTableViewController.swift
@@ -112,43 +112,40 @@ class SavedGamesTableViewController: FetchResultsTableViewController {
         
         return savedGameCell;
     }
-    //"2017-03-15"
-    //"9:45 AM"
+    
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            guard
+                let context = fetchedResultsController?.managedObjectContext,
+                let chessGameMO = fetchedResultsController?.object(at: indexPath) as? ChessGameMO
+                else {print("Error retrieving context, or object to delete");return}
+            context.perform {
+                context.delete(chessGameMO)
+                do {
+                    try context.save()
+                } catch let error as NSError {
+                    print("Error saving context:\(error)")
+                }
+            }
+        }
     }
-    */
+    
 
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+   
 
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
+    
 
     
     // MARK: - Navigation
